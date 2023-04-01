99ba9a0c254a853dafee0065039cb3f52980b585
Sebastian Duran <sd8587793@gmail.com>
2023-04-01 12:12:38 -0400

    03 - Agregar una tarea a la lista

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
 const btn = document.querySelector("[data-form-btn]");
 const input = document.querySelector("[data-form-input]");
 
-/* //1ra manera de hacerlo
-btn.addEventListener("click", function (evento) {
-  //Con este metodo desactivo la recarga de la pagina al enviar un formulario
-  evento.preventDefault();
-  console.log("Hize click en el boton");
-  console.log(input.value);
-  //Con esto muestro cada evento que sufre el boton
-  //console.log(evento);
-});
-
-//2da manera de hacerlo
-btn.addEventListener("click", (evento) => {
-  //Con este metodo desactivo la recarga de la pagina al enviar un formulario
-  evento.preventDefault();
-  console.log("Hize click en el boton");
-  console.log(input.value);
-  //Con esto muestro cada evento que sufre el boton
-  //console.log(evento);
-}); */
-
 const crearTask = (evento) => {
-  evento.preventDefault();
-  console.log("Hize click en el boton");
-  console.log(input.value);
+  evento.preventDefault();  //Con este metodo desactivo la recarga de la pagina al enviar un formulario
+  const valorInput = input.value;
+  const task = document.createElement('li');
+  const list = document.querySelector('[data-list]');
+  list.appendChild(task);
+  task.classList.add('card');
+  //Aqui guardamos las etiquetas html que queramos insertar en comillas invertidas, y el valor dentro de estas lo ponemos con ${valorInput}
+  const contenidoTask = `<div>
+                      <i class="far fa-check-square icon"></i>
+                      <span class="task">${valorInput}</span>
+                     </div>
+                     <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  //Aqui el div task lo llenamos con el nuevo contenido de etiquetas con el metodo innertHTML
+  task.innerHTML = contenidoTask;
+  console.log(contenidoTask);
+  input.value = "";
 };
-btn.addEventListener('click', crearTask);
+btn.addEventListener('click', crearTask); //cuando yo haga click en el boton, va activar la funcion crearTask
