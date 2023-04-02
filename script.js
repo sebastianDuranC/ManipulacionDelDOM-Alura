e37e8dc20f3a03895fbcabe667b2c1a1078247b0
Sebastian Duran <sd8587793@gmail.com>
2023-04-02 11:03:37 -0400

    04 - Concluir una tarea

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-const btn = document.querySelector("[data-form-btn]");
-const input = document.querySelector("[data-form-input]");
+(() => {
+  const btn = document.querySelector("[data-form-btn]");
+  const input = document.querySelector("[data-form-input]");
 
-const crearTask = (evento) => {
-  evento.preventDefault();  //Con este metodo desactivo la recarga de la pagina al enviar un formulario
-  const valorInput = input.value;
-  const task = document.createElement('li');
-  const list = document.querySelector('[data-list]');
-  list.appendChild(task);
-  task.classList.add('card');
-  //Aqui guardamos las etiquetas html que queramos insertar en comillas invertidas, y el valor dentro de estas lo ponemos con ${valorInput}
-  const contenidoTask = `<div>
-                      <i class="far fa-check-square icon"></i>
-                      <span class="task">${valorInput}</span>
-                     </div>
-                     <i class="fas fa-trash-alt trashIcon icon"></i>`;
-  //Aqui el div task lo llenamos con el nuevo contenido de etiquetas con el metodo innertHTML
-  task.innerHTML = contenidoTask;
-  console.log(contenidoTask);
-  input.value = "";
-};
-btn.addEventListener('click', crearTask); //cuando yo haga click en el boton, va activar la funcion crearTask
+  const crearTask = (evento) => {
+    evento.preventDefault(); //Con este metodo desactivo la recarga de la pagina al enviar un formulario
+    const valorInput = input.value;
+    const list = document.querySelector("[data-list]"); //Container mayor
+    const task = document.createElement("li"); //Container padre <li>
+    task.classList.add("card");
+    list.appendChild(task);
+
+    const taskContent = document.createElement("div"); //Container hijo <div> del padre <li>
+    taskContent.appendChild(checkComplete()); // Aqui lleno con una etiqueta <i> que es el icono de checkpoint
+
+    const tittleTasks = document.createElement("span"); //hijo <span> del container <diV> del taskContent
+    tittleTasks.classList.add("task");
+    tittleTasks.innerHTML = valorInput;
+    
+    taskContent.appendChild(tittleTasks);
+    const contenidoTask = `<i class="fas fa-trash-alt trashIcon icon"></i>`;
+    //Aqui el div task lo llenamos con el nuevo contenido de etiquetas con el metodo innertHTML
+    task.appendChild(taskContent);
+    list.appendChild(task);
+    input.value = "";
+  };
+  btn.addEventListener("click", crearTask); //cuando yo haga click en el boton, va activar la funcion crearTask
+
+  const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
+    return i;
+  };
+
+  const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+  };
+})();
